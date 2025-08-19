80214081a31d09c016d405f55999d080e1a6d537
simonb0t <simon.ago.2421@gmail.com>
2025-08-19 04:38:14 -0400

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
@@ -12,5 +12,5 @@ def webhook():
     print("Mensaje recibido:", data)
     return "OK", 200
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
