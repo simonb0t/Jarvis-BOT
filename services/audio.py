f75b909b545ca929a379aa5b3bffdd3ecc3935d1
simonb0t <simon.ago.2421@gmail.com>
2025-08-21 14:22:45 -0400

    Update audio.py

--- services/audio.py
+++ services/audio.py
@@ -1,1 +1,13 @@
+import speech_recognition as sr
+
+def audio_to_text(file_path):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(file_path) as source:
+        audio = recognizer.record(source)
+    try:
+        return recognizer.recognize_google(audio, language="es-ES")
+    except sr.UnknownValueError:
+        return "No se entendió el audio."
+    except sr.RequestError:
+        return "Error con el servicio de reconocimiento."
 
