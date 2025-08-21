2e33b7f714dbf4ac601da21165bcacac264adb91
simonb0t <simon.ago.2421@gmail.com>
2025-08-21 14:32:13 -0400

    Delete index.py

--- index.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from modules.whatsapp_module import whatsapp_bp
-
-app = Flask(__name__)
-app.register_blueprint(whatsapp_bp)
-
-@app.route("/")
-def home():
-    return "🤖 Jarvis WhatsApp activo."
